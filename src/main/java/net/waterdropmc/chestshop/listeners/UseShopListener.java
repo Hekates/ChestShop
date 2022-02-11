80f7c288adf86716c1ce2f757d377beca0401c01
Hekates <64363316+Hekates@users.noreply.github.com>
2022-02-11 21:30:18 +0100

    kein plan tbh

--- src/main/java/net/waterdropmc/chestshop/listeners/UseShopListener.java
+++ src/main/java/net/waterdropmc/chestshop/listeners/UseShopListener.java
@@ -21,12 +21,8 @@ public class UseShopListener implements Listener {
         Configuration config = ChestShop.getPlugin(ChestShop.class).getConfig();
 
         Player player = event.getPlayer();
-        //Checks if the handitem could contain a shop tag
-        if (!player.getInventory().getItemInHand().getType().equals(Material.AIR)) {
-            if (player.getInventory().getItemInHand().getItemMeta().hasDisplayName()) {
-                if (player.getInventory().getItemInHand().getItemMeta().getDisplayName().contains("shop")) return;
-            }
-        }
+
+        if (!player.getInventory().getItemInHand().getType().equals(Material.AIR)) return;
 
         ItemFrame itemFrame = (ItemFrame) event.getRightClicked();
         ItemStack item = itemFrame.getItem();
@@ -34,43 +30,52 @@ public class UseShopListener implements Listener {
         ItemStack itemStackMat = new ItemStack(item.getType());
 
         Block attachedBlock = itemFrame.getLocation().getBlock().getRelative(itemFrame.getAttachedFace());
-        Chest chest = (Chest) attachedBlock.getLocation().getBlock().getState();
+        if (attachedBlock.getType().equals(Material.CHEST)) {
+            Chest chest = (Chest) attachedBlock.getLocation().getBlock().getState();
 
-        int amount = Integer.parseInt(item.getItemMeta().getLore().get(0));
-        int price = Integer.parseInt(item.getItemMeta().getLore().get(1));
-        String currency = item.getItemMeta().getLore().get(2);
+            int amount = Integer.parseInt(item.getItemMeta().getLore().get(0));
+            int price = Integer.parseInt(item.getItemMeta().getLore().get(1));
+            String currency = item.getItemMeta().getLore().get(2);
 
-        ItemStack currancyStack = new ItemStack(Material.matchMaterial(currency));
+            ItemStack currancyStack = new ItemStack(Material.matchMaterial(currency));
 
-        if (!event.getRightClicked().equals(EntityType.ITEM_FRAME)) return;
+            if (!event.getRightClicked().equals(EntityType.ITEM_FRAME)) return;
 
-        if (itemFrame.getItem().getType().equals(Material.AIR)) return;
+            if (itemFrame.getItem().getType().equals(Material.AIR)) return;
+            if (!itemFrame.getItem().getItemMeta().getLore().contains("shop")) return;
+            player.sendMessage("45");
 
-        //Checks if the Chest has enough Items to sell
-        if (!chest.getBlockInventory().containsAtLeast(itemStackMat, amount)) {
-            player.sendMessage(config.getString("not-enough-items-in-chest-buy-error"));
-            return;
-        }
-        //Checks if player has the permission
-        if (!player.hasPermission("cs.buy")){
-            player.sendMessage(config.getString("boy-permission-error"));
-            return;
-        }
-        //Checks if player has enough playment
-        if (!player.getInventory().containsAtLeast(currancyStack, price)){
-            player.sendMessage(config.getString("not-enough-payment-error"));
-            return;
-        }
-        //Checks if has an empty slot and then executes the transaction
-        if (player.getInventory().firstEmpty() != -1) {
-            itemStackMat.setAmount(amount);
-            currancyStack.setAmount(price);
+            //Checks if the Chest has enough Items to sell
+            if (!chest.getBlockInventory().containsAtLeast(itemStackMat, amount)) {
+                player.sendMessage(config.getString("not-enough-items-in-chest-buy-error"));
+                return;
+            }
+            player.sendMessage("52");
+            //Checks if player has the permission
+            if (!player.hasPermission("cs.buy")) {
+                player.sendMessage(config.getString("boy-permission-error"));
+                return;
+            }
+            player.sendMessage("58");
+            //Checks if player has enough playment
+            if (!player.getInventory().containsAtLeast(currancyStack, price)) {
+                player.sendMessage(config.getString("not-enough-payment-error"));
+                return;
+            }
+            player.sendMessage("64");
+            //Checks if has an empty slot and then executes the transaction
+            if (player.getInventory().firstEmpty() != -1) {
+                itemStackMat.setAmount(amount);
+                currancyStack.setAmount(price);
 
-            player.getInventory().addItem(itemStackMat);
-            chest.getInventory().remove(itemStackMat);
-            player.getInventory().remove(currancyStack);
-        } else {
-            player.sendMessage(config.getString("no-space-error"));
+                player.getInventory().addItem(itemStackMat);
+                chest.getInventory().remove(itemStackMat);
+                player.getInventory().remove(currancyStack);
+                player.sendMessage("73");
+            } else {
+                player.sendMessage(config.getString("no-space-error"));
+                player.sendMessage("76");
+            }
         }
     }
 }
